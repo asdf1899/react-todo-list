019f3438842aa0108f5a53a8847eb02d91e2f262
asdf1899 <araid.anas99@gmail.com>
2019-03-16 22:54:26 +0100

    Add todos const

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const todos = {
+  items: [],
+  get(){
+    return items;
+  },
+  add(obj){
+    this.items.push(obj);
+  },
+  remove (id) {
+    this.items.splice(id, 1);
+    this.save();
+  }
+};
+
 class App extends Component {
+  constructor (props) {
+      super(props);
+      this.state = {
+          todos: todos.items
+      };
+  }
   render() {
     return (
-      <div className="App">
-       
+      <div>
+        <h1>Simple reactJS todo list</h1>
+        <CreateTodo
+          createTask={this.createTask.bind(this)}
+        />
       </div>
     );
   }
