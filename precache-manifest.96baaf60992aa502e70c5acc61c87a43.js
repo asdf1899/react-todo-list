b86099785a4dfee30e6521381c03c34b19680213
asdf1899 <araid.anas99@gmail.com>
2019-03-17 01:32:17 +0100

    Updates

---
+++ precache-manifest.96baaf60992aa502e70c5acc61c87a43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f505454316874baf61dc",
+    "url": "/asdf1899.github.io/react-todo-list/static/js/runtime~main.1f8d1a4e.js"
+  },
+  {
+    "revision": "ff0ac2da30e8c69f972a",
+    "url": "/asdf1899.github.io/react-todo-list/static/js/main.840e265f.chunk.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/asdf1899.github.io/react-todo-list/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "ff0ac2da30e8c69f972a",
+    "url": "/asdf1899.github.io/react-todo-list/static/css/main.34d8cbfa.chunk.css"
+  },
+  {
+    "revision": "f48d3f4c642f2672e34e0fecda2a0ba6",
+    "url": "/asdf1899.github.io/react-todo-list/index.html"
+  }
+];
